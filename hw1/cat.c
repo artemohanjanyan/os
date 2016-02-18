03438f78f55a1d646baabc677feb8571b1bd3e9b
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2016-02-18 18:46:45 +0300

    refactor

--- hw1/cat.c
+++ hw1/cat.c
@@ -1,35 +1,49 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+//#include <sys/types.h>
+//#include <sys/stat.h>
+#include <fcntl.h>
 
-int main()
+int const BUF_SIZE = 1024;
+
+void cat(int infd, int outfd, int errfd)
 {
-	char buf[1024];
+	char buf[BUF_SIZE];
 	int length;
-	int i;
-	while (1)
+	while ((length = read(infd, buf, BUF_SIZE)) > 0)
 	{
-		length = read(0, buf, 1024);
-		if (length == -1)
-		{
-			write(2, "read error\n", 11);
-			return 0;
-		}
-
-		if (length == 0)
-			return 0;
-
-		i = 0;
+		int i = 0;
 		while (i < length)
 		{
-			int ret = write(1, buf + i, length - i);
+			int ret = write(outfd, buf + i, length - i);
 			if (ret == -1)
 			{
-				write(2, "write error\n", 12);
-				return 0;
+				printf("Error during write(): %s\n", strerror(errno));
+				return;
 			}
 
 			i += ret;
 		}
 	}
+
+	if (length == -1)
+	{
+		printf("Error during read(): %s\n", strerror(errno));
+		return;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	int infd = 0;
+	if (argc > 1)
+		infd = open(argv[1], O_RDONLY);
+
+	cat(infd, 1, 2);
+
+	if (infd != 0)
+		close(infd);
 
 	return 0;
 }

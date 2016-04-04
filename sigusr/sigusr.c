84218b86b26ae4a3f802963317e38195d8ce9bdd
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2016-04-05 02:51:43 +0300

    sleep -> alarm

--- sigusr/sigusr.c
+++ sigusr/sigusr.c
@@ -7,10 +7,20 @@
 int handled_signal = 0; // 0 is not a signal number
 pid_t origin_pid;
 
+int alarm_triggered = 0;
+
 void handler(int signum, siginfo_t *siginfo, void *_)
 {
-	handled_signal = signum;
-	origin_pid = siginfo->si_pid;
+	if (signum != SIGALRM)
+	{
+		if (handled_signal == 0)
+		{
+			handled_signal = signum;
+			origin_pid = siginfo->si_pid;
+		}
+	}
+	else
+		alarm_triggered = 1;
 }
 
 int main()
@@ -19,13 +29,18 @@ int main()
 	act.sa_flags = SA_SIGINFO;
 	act.sa_sigaction = &handler;
 
-	if (sigemptyset(&act.sa_mask) || sigaddset(&act.sa_mask, SIGUSR1) || sigaddset(&act.sa_mask, SIGUSR2))
+	if (sigemptyset(&act.sa_mask)
+			|| sigaddset(&act.sa_mask, SIGUSR1) || sigaddset(&act.sa_mask, SIGUSR2)
+			|| sigaddset(&act.sa_mask, SIGALRM))
 		fprintf(stderr, "Error during sig...set(): %s\n", strerror(errno));
 
-	if (sigaction(SIGUSR1, &act, NULL) || sigaction(SIGUSR2, &act, NULL))
+	if (sigaction(SIGUSR1, &act, NULL) || sigaction(SIGUSR2, &act, NULL) || sigaction(SIGALRM, &act, NULL))
 		fprintf(stderr, "Error during sigaction(): %s\n", strerror(errno));
 
-	sleep(10);
+	alarm(10);
+	while (alarm_triggered == 0)
+		pause();
+	//sleep(10);
 
 	if (handled_signal == 0)
 		printf("No signals were caught\n");

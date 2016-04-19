f5421a74986fac570ce5f510e91764fee93f45f1
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2016-04-19 12:01:10 +0300

    one test instead of 3

--- badlinks/badlinks.sh
+++ badlinks/badlinks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in `find -L $1`
 do
-	if test -L "$i" && ! test -e "$i" && [ $(($(date '+%s') - $(stat -c '%Y' "$i"))) -ge $((60*60*24*7)) ]
+	if test -L "$i" -a ! -e "$i" -a $(($(date '+%s') - $(stat -c '%Y' "$i"))) -ge $((60*60*24*7))
 	then
 		echo "$i"
 	fi

2f234096476dadc3d5783a4f16bbf87479c86f60
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2016-05-16 00:06:08 +0300

    almost done

--- rshd/rshd.c
+++ rshd/rshd.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/epoll.h>
+#include <sys/ioctl.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -10,30 +11,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-int const BUF_SIZE = 1024;
-int const MAX_EVENTS = 1024;
+#define BUF_SIZE 1024
+#define MAX_EVENTS 10
 
 char buf[BUF_SIZE];
-
-int make_non_blocking(int fd)
-{
-	int flags = fcntl(fd, F_GETFL, 0);
-	return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
-}
 
 struct event_data;
 struct client_data;
 
-struct pipes_data
+struct pty_data
 {
-	int read_pipe[2], write_pipe[2];
+	int master_fd;
 	client_data *client;
 };
 
 struct client_data
 {
 	int client_fd;
-	pipes_data *pipes;
+	pty_data *pty;
 };
 
 enum data_type
@@ -47,13 +42,13 @@ struct event_data
 
 	union
 	{
-		int             *server;
-		client_data     *client;
-		pipes_data *pipes;
+		int         *server;
+		client_data *client;
+		pty_data    *pty;
 	};
 };
 
-int fork_term(pipes_data *pipes)
+int fork_term(int master_fd, int slave_fd)
 {
 	int child_pid;
 	child_pid = fork();
@@ -62,18 +57,21 @@ int fork_term(pipes_data *pipes)
 
 	if (child_pid == 0)
 	{
-		bool dup_fail = dup2(pipes->read_pipe[0],  STDIN_FILENO) == -1 ||
-		                dup2(pipes->write_pipe[1], STDOUT_FILENO) == -1;
-		close(pipes->read_pipe[0]);
-		close(pipes->read_pipe[1]);
-		close(pipes->write_pipe[0]);
-		close(pipes->write_pipe[1]);
-		if (!dup_fail)
+		bool dup_fail = dup2(slave_fd, STDIN_FILENO) == -1 ||
+		                dup2(slave_fd, STDOUT_FILENO) == -1 ||
+							 dup2(slave_fd, STDERR_FILENO) == -1;
+		close(master_fd);
+		close(slave_fd);
+		ioctl(0, TIOCSCTTY, 1);
+		if (!dup_fail && setsid() != -1)
 			execlp("/bin/sh", "/bin/sh", NULL);
-		return 0;
+		exit(0);
 	}
 	else
+	{
+		close(slave_fd);
 		return child_pid;
+	}
 }
 
 int main(int argc, char *argv[])
@@ -94,25 +92,23 @@ int main(int argc, char *argv[])
 		{
 			printf("Wrong argument format.\n");
 			printf("Usage: ./rshd <port>\n");
-			goto server_destruct;
-			//address.sin_port = htons(2539);
+			goto close_server;
 		}
 
 		int enable = 1;
 		setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(enable));
-		//make_non_blocking(server_fd);
 
 		if (bind(server_fd, reinterpret_cast<sockaddr*>(&address), sizeof address) == -1)
 		{
 			printf("Error during bind: %s\n", strerror(errno));
-			goto server_destruct;
+			goto close_server;
 		}
 	}
 
 	if (listen(server_fd, SOMAXCONN) == -1)
 	{
 		printf("Error during listen: %s\n", strerror(errno));
-		goto server_destruct;
+		goto close_server;
 	}
 
 	/* -----------
@@ -123,7 +119,7 @@ int main(int argc, char *argv[])
 	if ((epoll_fd = epoll_create1(0)) == -1)
 	{
 		printf("Error during epoll_create1: %s\n", strerror(errno));
-		goto server_destruct;
+		goto close_server;
 	}
 
 	event_data server_data;
@@ -134,25 +130,19 @@ int main(int argc, char *argv[])
 	event.events = EPOLLIN | EPOLLRDHUP;
 	event.data.ptr = &server_data;
 
-	epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event);
+	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &event) == -1)
+		goto close_epoll;
 
-	/* -----------------------
-		Communicate with client
-	   ----------------------- */
-
-	//int client_fd;
-	//if ((client_fd = accept(server_fd, NULL, NULL)) == -1)
-	//{
-	//	printf("Error during accept: %s\n", strerror(errno));
-	//	goto server_destruct;
-	//}
-
-	//make_non_blocking(client_fd);
-
-	//printf("Connected\n");
+	/* ------------------------
+		Communicate with clients
+	   ------------------------ */
 
 	epoll_event events[MAX_EVENTS];
 	while (int event_n = epoll_wait(epoll_fd, events, MAX_EVENTS, -1))
+	{
+		void *deleted_ptrs[MAX_EVENTS * 2];
+		int deleted_ptrs_cnt = 0;
+
 		for (int event_i = 0; event_i < event_n; ++event_i)
 		{
 			event_data *data = (event_data*) events[event_i].data.ptr;
@@ -160,100 +150,161 @@ int main(int argc, char *argv[])
 			{
 				case SERVER:
 				{
-					client_data *client = (client_data*) malloc(sizeof(client_data));
-					pipes_data  *pipes  = (pipes_data*)  malloc(sizeof(pipes_data));
+					int master_fd, slave_fd;
+					if ((master_fd = posix_openpt(O_RDWR)) == -1 ||
+							grantpt(master_fd) == -1 ||
+							unlockpt(master_fd) == -1 ||
+							(slave_fd = open(ptsname(master_fd), O_RDWR)) == -1)
+						goto close_master;
 
-					client->pipes = pipes;
-					pipes->client = client;
-
-					client->client_fd = accept(server_fd, NULL, NULL);
-
-					if (pipe(pipes->read_pipe) == -1)
-						break;
-					if (pipe(pipes->write_pipe) == -1)
+					if (fork_term(master_fd, slave_fd) == -1)
 					{
-						close(pipes->read_pipe[0]);
-						close(pipes->read_pipe[1]);
+						close(slave_fd);
+						goto close_master;
 					}
 
-					fork_term(pipes);
+					client_data *client;
+					client = (client_data*) malloc(sizeof(client_data));
+					pty_data *pty;
+					pty = (pty_data*)    malloc(sizeof(pty_data));
+
+					client->pty = pty;
+					pty->client = client;
+
+					client->client_fd = accept(server_fd, NULL, NULL);
+					pty->master_fd = master_fd;
+
+					if (client->client_fd == -1)
+						goto free_data;
 
 					data = (event_data*) malloc(sizeof(event_data));
+					if (data == NULL)
+						goto close_client;
 					data->type = CLIENT;
 					data->client = client;
 					event.data.ptr = data;
-					epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client->client_fd, &event);
+					if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client->client_fd, &event) == -1)
+						goto free_client_data;
 
 					data = (event_data*) malloc(sizeof(event_data));
+					if (data == NULL)
+						goto unregister_client;
 					data->type = CONNECTION;
-					data->pipes = pipes;
+					data->pty = pty;
 					event.data.ptr = data;
-					epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pipes->write_pipe[0], &event);
+					if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pty->master_fd, &event) == -1)
+						goto free_pty_data;
 
+					goto server_finally;
+
+				free_pty_data:
+					free(data);
+				unregister_client:
+					epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client->client_fd, NULL);
+				free_client_data:
+					free(data);
+				close_client:
+					close(client->client_fd);
+				free_data:
+					free(client);
+					free(pty);
+				close_master:
+					if (master_fd != -1)
+						close(master_fd);
+					printf("Cant open pty: %s\n", strerror(errno));
+
+				server_finally:
 					break;
 				}
 
 				case CLIENT:
 				{
+					int deleted_ptrs_it = 0;
+					for (; deleted_ptrs_it < deleted_ptrs_cnt; ++deleted_ptrs_it)
+						if (events[event_i].data.ptr == deleted_ptrs[deleted_ptrs_it])
+							break;
+					if (deleted_ptrs_it < deleted_ptrs_cnt &&
+							events[event_i].data.ptr == deleted_ptrs[deleted_ptrs_it])
+						break;
+
 					if (events[event_i].events & EPOLLIN)
 					{
 						ssize_t length = read(data->client->client_fd, buf, BUF_SIZE);
 						ssize_t written = 0;
 						while (written < length)
 						{
-							ssize_t n = write(data->client->pipes->read_pipe[1], buf + written, length - written);
+							ssize_t n = write(data->client->pty->master_fd, buf + written, length - written);
 							if (n != -1)
 								written += n;
 							else
 								break;
 						}
 					}
-					else
+
+					if (events[event_i].events & ~EPOLLIN)
 					{
 						epoll_ctl(epoll_fd, EPOLL_CTL_DEL, data->client->client_fd, NULL);
+						epoll_ctl(epoll_fd, EPOLL_CTL_DEL, data->client->pty->master_fd, NULL);
 						close(data->client->client_fd);
-						close(data->client->pipes->read_pipe[0]);
-						close(data->client->pipes->read_pipe[1]);
+						close(data->client->pty->master_fd);
+
+						deleted_ptrs[deleted_ptrs_cnt++] = data->client->pty;
+
+						free(data->client->pty);
 						free(data->client);
 						free(data);
-
-						fprintf(stderr, "client rdhup");
 					}
+
 					break;
 				}
 
 				case CONNECTION:
 				{
+					int deleted_ptrs_it = 0;
+					for (; deleted_ptrs_it < deleted_ptrs_cnt; ++deleted_ptrs_it)
+						if (events[event_i].data.ptr == deleted_ptrs[deleted_ptrs_it])
+							break;
+					if (deleted_ptrs_it < deleted_ptrs_cnt &&
+							events[event_i].data.ptr == deleted_ptrs[deleted_ptrs_it])
+						break;
+
 					if (events[event_i].events & EPOLLIN)
 					{
-						ssize_t length = read(data->pipes->write_pipe[0], buf, BUF_SIZE);
+						ssize_t length = read(data->pty->master_fd, buf, BUF_SIZE);
 						ssize_t written = 0;
 						while (written < length)
 						{
-							ssize_t n = write(data->pipes->client->client_fd, buf + written, length - written);
+							ssize_t n = write(data->pty->client->client_fd, buf + written, length - written);
 							if (n != -1)
 								written += n;
 							else
 								break;
 						}
 					}
-					else
-					{
-						epoll_ctl(epoll_fd, EPOLL_CTL_DEL, data->pipes->write_pipe[0], NULL);
-						close(data->pipes->write_pipe[0]);
-						close(data->pipes->write_pipe[1]);
-						free(data->pipes);
-						free(data);
 
-						fprintf(stderr, "connection rdhup");
+					if (events[event_i].events & ~EPOLLIN)
+					{
+						epoll_ctl(epoll_fd, EPOLL_CTL_DEL, data->pty->master_fd, NULL);
+						epoll_ctl(epoll_fd, EPOLL_CTL_DEL, data->pty->client->client_fd, NULL);
+						close(data->pty->master_fd);
+						close(data->pty->client->client_fd);
+
+						deleted_ptrs[deleted_ptrs_cnt++] = data->pty->client;
+
+						free(data->client->pty);
+						free(data->client);
+						free(data);
 					}
+
 					break;
 				}
 			}
 		}
+	}
 
+close_epoll:
 	close(epoll_fd);
-server_destruct:
+close_server:
 	close(server_fd);
 
 	return 0;
